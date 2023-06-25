a950ba1ed7e9269cb29bd06cb03e30383eb74872
Nada Alsaid <ns1062321@gmail.com>
2023-06-26 01:02:35 +0300

    upload

---
+++ Modulo Sum/[TEMPLATE]/ModuloSum/ModuloSum.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    // *****************************************
+    // DON'T CHANGE CLASS OR FUNCTION NAME
+    // YOU CAN ADD FUNCTIONS IF YOU NEED TO
+    // *****************************************
+    public static class ModuloSum
+    {
+        #region YOUR CODE IS HERE    
+
+        #region FUNCTION#1: Calculate the Value
+        //Your Code is Here:
+        //==================
+        /// <summary>
+        /// Fill this function to check whether there's a subsequence numbers in the given array that their sum is divisible by M
+        /// </summary>
+        /// <param name="items">array of integers</param>
+        /// <param name="N">array size </param>
+        /// <param name="M">value to check against it</param>
+        /// <returns>true if there's subsequence with sum divisible by M... false otherwise</returns>
+        static List<int> numbers;
+        static bool isSubsequence;
+        static Dictionary<int, int> dic;
+        static public bool SolveValue(int[] items, int N, int M)
+        {
+            //DC Code
+            /*
+        //if (N==0)
+        //    return false;
+        //if (items[N-1] == M)
+        //    return true;
+        //if (M == 0)
+        //    return true;
+        //if (items[N-1 ]  != M)
+        //{
+        //    B1 = SolveValue(items, N - 1,(M - items[N-1]) %M );
+        //    B2 = SolveValue(items, N - 1, M);
+        //    return B1 || B2;
+        //}
+        //else
+        //{
+        //    return SolveValue(items, N - 1, M);
+        //}
+        */
+
+            dic = new Dictionary<int, int>(); // To make sure that I insert item once
+            bool[,] R = new bool[N + 1, M + 1]; // To certain there is sum of number divided on M or not
+            numbers = new List<int>(); //To sort items that divided on M
+            isSubsequence = false;
+            //if N=1 or m=1 , check first item if it divided on M or not 
+            if (N == 1 || M == 1)
+            {
+                isSubsequence = (items[0] % M == 0);
+                if (isSubsequence)
+                {
+                    numbers = new List<int>
+                    {
+                        items[0]
+                    };
+                }
+                return isSubsequence;
+            }
+            //check items if there is sum of numbers divided on M or not 
+            for (int i = 0; i <= N; i++)
+            {
+                for (int j = 1; j <= M; j++)
+                {
+                    if (i == 0)
+                    {
+                        R[i, j] = true;
+                    }
+                    else if (items[i - 1] != j )
+                    {
+                        R[i, j] = R[i - 1, Math.Abs((j - items[i - 1])) % M] || R[i - 1, j];
+                        
+                        if (  items[i - 1]   % M == 0  )
+                        {
+                            
+                            numbers = new List<int>{
+                                    items[i - 1]
+                            };
+
+                            isSubsequence = true;
+                            return true;
+                        }
+
+                        if (R[i - 1, Math.Abs((j - items[i - 1])) % M] && !dic.ContainsKey(i)&& ( items[i - 1] - j) > 0 )
+                        {
+                            dic.Add(i, items[i - 1]);
+                            numbers.Add(items[i - 1]);
+                        }
+                    }
+                    else
+                    {
+                        R[i, j] = R[i-1, j];
+                       
+                    }
+                }
+
+            }
+            isSubsequence = R[N, M];
+            return isSubsequence;
+        }
+        #endregion
+
+        #region FUNCTION#2: Construct the Solution
+        //Your Code is Here:
+        //==================
+        /// <returns>if exists, return the numbers themselves whose sum is divisible by ‘M’ else, return null</returns>
+        static public int[] ConstructSolution(int[] items, int N, int M)
+        {
+        
+            if (!isSubsequence)
+                return null;
+            return numbers.ToArray();
+        }
+
+
+        #endregion
+        #endregion
+    }
+}
---
+++ OfficeOrganization/[TEMPLATE]/OfficeOrganization/OfficeOrganization.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    // *****************************************
+    // DON'T CHANGE CLASS OR FUNCTION NAME
+    // YOU CAN ADD FUNCTIONS IF YOU NEED TO
+    // *****************************************
+    public static class OfficeOrganization
+    {
+        #region YOUR CODE IS HERE
+        /// <summary>
+        /// find the minimum costs in MOST EFFICIENT WAY to organize your office to meet your father needs.
+        /// </summary>
+        /// <param name="N">initial load</param>
+        /// <param name="M">target load required by your father</param>
+        /// <param name="A">cost of reducing the load by half</param>
+        /// <param name="B">cost of reducing the load by 1</param>
+        /// <returns>Min total cost to reduce the load from N to M</returns>
+        public static int OrganizeTheOffice(int N, int M, int A, int B)
+        {
+            int result = 0;
+            while (N > M)
+            {
+                if ((N - M) * B > (A + (N/2 - M) * B) && N/2 >= M)
+                {
+                    N /= 2;
+                    result += A;
+                }
+                else
+                {
+                    result += (N - M) * B;
+                    N = M ;
+                }
+                
+            }
+            return result;
+        }
+        #endregion
+    }
+}
